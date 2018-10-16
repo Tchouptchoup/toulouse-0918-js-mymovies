c08aa4c54773010587330ff000a94e330455fa6a
Maitetxu Darguy <maitetxu.darguy@gmail.com>
2018-10-16 16:58:35 +0200

    début modifs router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
-import {APIKey} from './settings'
-import Slider from "./Slider"
+
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
@@ -10,15 +11,11 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-        
           <Navbar/> 
         </header>
-        <h3>Bientôt à l'affiche</h3>
-        <Slider url={`https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKey}&language=fr-FR&page=1`}/>
-        <h3>Les plus populaires</h3>
-        <Slider url={`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=fr-FR&page=1`}/>
-        <h3>Les mieux notés</h3>
-        <Slider url={`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKey}&language=fr-FR&page=1`}/>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
         <footer>
           <Footer />
         </footer>
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import {APIKey} from './settings'
+import Slider from "./Slider"
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Bientôt à l'affiche</h3>
+        <Slider url={`https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKey}&language=fr-FR&page=1`}/>
+        <h3>Les plus populaires</h3>
+        <Slider url={`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=fr-FR&page=1`}/>
+        <h3>Les mieux notés</h3>
+        <Slider url={`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKey}&language=fr-FR&page=1`}/>
+      </div>
+    );
+  }
+}
+
+export default Home;
